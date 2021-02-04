6c60c3f003129b54787690cd0cb9018229881512
Thomas Temple <tjt_84@hotmail.com>
2021-02-04 19:02:23 +1100

    added a bike show page skeleton

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,7 +1,17 @@
 class BikesController < ApplicationController
+  # before_action :bike_params only: [:show]
+
+
   def index
   end
 
   def show
+    @bike = Bike.find(params[:id])
+  end
+
+  private
+
+  def bike_params
+    @bike = Bike.find(params[:id])
   end
 end
