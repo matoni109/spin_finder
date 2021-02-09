25c91b31e053f2cd87c418d51169c1c0e3912535
Julian Harrington <jrayharrington@gmail.com>
2021-02-09 13:08:07 +1100

    introduced search bar to Mapbox map, changed theme to dark, updated application.scss to include default import for mapbox

--- app/javascript/packs/init_mapbox.js
+++ app/javascript/packs/init_mapbox.js
@@ -1,13 +1,15 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const buildMap = (mapElement) => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     return new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v10'
+        style: 'mapbox://styles/mapbox/dark-v10'
     });
 };
 
+// Add markers to the map
 const addMarkersToMap = (map, markers) => {
     markers.forEach((marker) => {
         const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
@@ -31,7 +33,11 @@ const initMapbox = () => {
         const markers = JSON.parse(mapElement.dataset.markers);
         addMarkersToMap(map, markers);
         fitMapToMarkers(map, markers);
+
+        // For Search function on your map
+        map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl }));
     }
-};
+  };
 
 export { initMapbox };
