65f5ea12c17283aebbf721021986f37b0189d37e
Chris Wade <chris@wadespace.net>
2021-02-13 16:04:24 +1100

    done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,9 @@ module ApplicationHelper
   end
 
   ### on bikes show page
-      def cloudinary_imgs_cara(key,instance)
+     def cloudinary_imgs_cara(key,instance)
     cl_image_tag(key, :quality=>"auto", :fetch_format=>:auto, :crop=>"fit", :class=>"d-block mx-auto img-fluid rounded", :alt=>"#{instance.name}")
   end
-
   def cloudinary_avatar_nav(key)
     cl_image_tag(key, :height=>250, :width=>350, :fetch_format=>:auto, :crop=>"crop", :class=>"avatar dropdown-toggle")
   end
